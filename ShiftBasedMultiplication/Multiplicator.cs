c317d599ab563f9f259320321378a633e902cb55
Dmytro Grablov <dmytro.grablov@gmail.com>
2019-12-12 13:28:46 +0000

    added multiplicator for unsigned longs

---
+++ ShiftBasedMultiplication/Multiplicator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShiftBasedMultiplication
+{
+    class Multiplicator
+    {
+        public static ulong MultiplyUnsigned(ulong operandA, ulong operandB)
+        {
+            ulong result = 0x00;
+            for (sbyte i = 0; i < 64; i++)
+            {
+                ulong mask = ((operandB >> i) & 0x1) == 0x1 ? 0xffffffffffffffff : 0x0;
+                result += (operandA << i) & mask;
+            }
+            return result;
+        }
+    }
+}
--- ShiftBasedMultiplication/Program.cs
+++ ShiftBasedMultiplication/Program.cs
@@ -6,7 +6,17 @@ namespace ShiftBasedMultiplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Write("enter operand A: ");
+                string inputA = Console.ReadLine();
+                ulong operandA = UInt64.Parse(inputA);
+                Console.Write("enter operand B: ");
+                string inputB = Console.ReadLine();
+                ulong operandB = UInt64.Parse(inputB);
+                ulong result = Multiplicator.MultiplyUnsigned(operandA, operandB);
+                Console.WriteLine("multiplication result is {0}, and should be {1}", result, operandA * operandB);
+            }
         }
     }
 }
