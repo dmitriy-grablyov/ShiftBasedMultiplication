654b3d8f559acbcdfecd445b2d3c09df8af40be7
Dmytro Grablov <dmytro.grablov@gmail.com>
2019-12-12 13:02:02 +0000

    initial setup

---
+++ ShiftBasedMultiplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShiftBasedMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
