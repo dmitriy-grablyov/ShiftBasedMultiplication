ec1c3b5a69fffa9da49439462d8d13648a0378cb
Dmytro Grablov <dmytro.grablov@gmail.com>
2019-12-12 14:43:43 +0000

    multiplication with explanation

--- ShiftBasedMultiplication/Multiplicator.cs
+++ ShiftBasedMultiplication/Multiplicator.cs
@@ -19,5 +19,40 @@ namespace ShiftBasedMultiplication
             } while (operandB > 0);
             return result;
         }
+
+        public uint MultiplyUnsignedExplained(uint operandA, uint operandB)
+        {
+            uint result = 0x00;
+            Console.WriteLine();
+            Console.WriteLine("Last bit of the right operand determines, whether current state of the left operand must be added to accumulator");
+            Console.WriteLine("after each iteration, left operand is shifted left, and right one is shifted right");
+            Console.WriteLine("A is :{0}, ({1})", getBitRepresentation(operandA), operandA);
+            Console.WriteLine("B is :{0}, ({1})", getBitRepresentation(operandB), operandB);
+            Console.WriteLine("{0,-35}{1,-37}{2,-35}{3,-35}", " Left operand", " Right Operand", " To add", " Accumulator");
+            Console.WriteLine("".PadLeft(128, '='));
+            do
+            {
+                Console.Write("{0,35}{1,37}", getBitRepresentation(operandA), getOperandBRepresentation(operandB));
+                uint mask = ((operandB) & 0x1) == 0x1 ? 0xffffffff : 0x0;
+                Console.Write("{0,35}", getBitRepresentation(operandA & mask));
+                result += operandA & mask;
+                Console.Write("{0,35} => {1}", getBitRepresentation(result), result);
+                operandA <<= 1;
+                operandB >>= 1;
+                Console.WriteLine();
+            } while (operandB > 0);
+            return result;
+        }
+
+        private string getOperandBRepresentation(uint operand)
+        {
+            string convertedOperand = getBitRepresentation(operand);
+            return convertedOperand.Substring(0, convertedOperand.Length - 1) + "{" + convertedOperand.Substring(convertedOperand.Length - 1) + "}";
+        }
+
+        private string getBitRepresentation(uint operand)
+        {
+            return "0b" + Convert.ToString(operand, 2).PadLeft(32, '0');
+        }
     }
 }
--- ShiftBasedMultiplication/Program.cs
+++ ShiftBasedMultiplication/Program.cs
@@ -6,15 +6,23 @@ namespace ShiftBasedMultiplication
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.SetWindowSize(140, Console.WindowWidth);
+            } catch (Exception ex)
+            {
+                Console.WriteLine("could not change size of the window, table might be displayed incorrectly");
+            }
             while (true)
             {
+                Multiplicator multiplicator = new Multiplicator();
                 Console.Write("enter operand A: ");
                 string inputA = Console.ReadLine();
                 uint operandA = UInt32.Parse(inputA);
                 Console.Write("enter operand B: ");
                 string inputB = Console.ReadLine();
                 uint operandB = UInt32.Parse(inputB);
-                uint result = Multiplicator.MultiplyUnsigned(operandA, operandB);
+                uint result = multiplicator.MultiplyUnsignedExplained(operandA, operandB);
                 Console.WriteLine("multiplication result is {0}, and should be {1}", result, operandA * operandB);
             }
         }
